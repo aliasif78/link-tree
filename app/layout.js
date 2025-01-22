c5b375913a9922f18ad1a6b9751a1fea371ae3ba
aliasif78 <aliasif1171@gmail.com>
2025-01-22 22:31:21 +0500

    Initial commit

---
+++ app/layout.js
@@ -0,0 +1,36 @@
+// Fonts
+import { Geist, Geist_Mono } from "next/font/google";
+
+// Components
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+
+// Styles
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Link Tree",
+  description: "A simple link tree for your social media profiles.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <NavBar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
